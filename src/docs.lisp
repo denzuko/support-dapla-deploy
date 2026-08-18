f41adda962ab79480f182e22180f05d244f053ca
Dwight Spencer <dwightaspencer@gmail.com>
2026-08-18 10:58:26 +0000

    Fix docs.lisp: replace stale invidious-* symbols with correct stoat-* symbols

--- src/docs.lisp
+++ src/docs.lisp
@@ -1,6 +1,7 @@
 ;;;; src/docs.lisp -- support-dapla-deploy/docs
 ;;;;
-;;;; 40ants-doc sections for the support-dapla-deploy system.
+;;;; Loaded as the :SUPPORT-DAPLA-DEPLOY/DOCS ASDF system, separate from
+;;;; the core deploy system so a production binary never pulls in 40ants-doc.
 
 (defpackage :support-dapla-deploy/docs
   (:use :cl)
@@ -14,16 +15,21 @@
   (@quadlet-builders section))
 
 (defsection @deploy-properties (:title "Consfigurator Properties")
-  (support-dapla-deploy/deploy:zfs-encryption-key        function)
-  (support-dapla-deploy/deploy:zfs-dataset-mounted       function)
-  (support-dapla-deploy/deploy:rootless-service-account  function)
-  (support-dapla-deploy/deploy:images-pulled             function)
-  (support-dapla-deploy/deploy:quadlets-activated        function)
-  (support-dapla-deploy/deploy:deploy-app                function))
+  (support-dapla-deploy/deploy:zfs-encryption-key       function)
+  (support-dapla-deploy/deploy:zfs-dataset-mounted      function)
+  (support-dapla-deploy/deploy:rootless-service-account function)
+  (support-dapla-deploy/deploy:images-pulled            function)
+  (support-dapla-deploy/deploy:quadlets-written         function)
+  (support-dapla-deploy/deploy:haproxy-vhost-written    function)
+  (support-dapla-deploy/deploy:quadlets-activated       function)
+  (support-dapla-deploy/deploy:deploy-app               function))
 
 (defsection @quadlet-builders (:title "Quadlet Unit Builders")
-  (support-dapla-deploy/deploy:cinix-write-string             function)
-  (support-dapla-deploy/deploy:invidious-network-sections     function)
-  (support-dapla-deploy/deploy:invidious-db-container-sections function)
-  (support-dapla-deploy/deploy:invidious-container-sections   function)
-  (support-dapla-deploy/deploy:haproxy-vhost-config           function))
+  (support-dapla-deploy/deploy:cinix-write-string              function)
+  (support-dapla-deploy/deploy:service-account-uid             function)
+  (support-dapla-deploy/deploy:stoat-network-sections          function)
+  (support-dapla-deploy/deploy:stoat-db-container-sections     function)
+  (support-dapla-deploy/deploy:stoat-cache-container-sections  function)
+  (support-dapla-deploy/deploy:stoat-files-container-sections  function)
+  (support-dapla-deploy/deploy:stoat-container-sections        function)
+  (support-dapla-deploy/deploy:haproxy-vhost-config            function))
